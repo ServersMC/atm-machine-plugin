f33691f3a523a1354cefdcd7ee2e76f7dd5eed97
Dennis Lysenko <dennislysenko@hotmail.com>
2015-06-10 04:56:56 -0400

    Removed Unused Import

--- src/org/croniks/atmmachine/types/ATMPlate.java
+++ src/org/croniks/atmmachine/types/ATMPlate.java
@@ -6,7 +6,6 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.bukkit.Location;
-import org.bukkit.entity.Player;
 import org.croniks.atmmachine.core.DataCon.Plates;
 
 public class ATMPlate {
